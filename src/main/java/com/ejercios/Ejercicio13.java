b2b541398d2a2ee7691157e12c10f7fd33225f17
Johann-Tafur <johanntafurfarfan@gmail.com>
2026-07-29 18:52:15 -0500

    Agregar Ejercicio13.java

---
+++ src/main/java/com/ejercios/Ejercicio13.java
@@ -0,0 +1,24 @@
+package com.ejercios;
+
+//Ejercicio 13
+
+//Calcular la suma de los primeros 100 números naturales.
+
+
+public class Ejercicio13 {
+    
+    public static void main(String[] args){
+
+        int Suma = 0;
+
+        for (int i = 1; i <= 100; i++){
+
+            Suma += i;
+            
+        }
+
+        System.out.print(Suma);
+
+    }
+
+}
