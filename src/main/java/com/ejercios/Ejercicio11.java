b6308ddf53e871b9e7345e6f8c8b8f75dcfa208f
Johann-Tafur <johanntafurfarfan@gmail.com>
2026-07-29 18:52:13 -0500

    Agregar Ejercicio11.java

---
+++ src/main/java/com/ejercios/Ejercicio11.java
@@ -0,0 +1,20 @@
+package com.ejercios;
+
+//PARTE III – Ciclos
+//Ejercicio 11
+
+//Mostrar los números del 1 al 100.
+
+public class Ejercicio11 {
+    
+    public static void main(String[] args){
+
+
+        for (int i = 1; i <= 100; i++){
+
+            System.out.println(i);
+
+        }
+    }
+
+}
